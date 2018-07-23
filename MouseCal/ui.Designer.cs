0e8678170e8e315c587170132eca30bd1e67ddd0
Lars <lt.lian@stud.uis.no>
2018-07-23 10:06:35 +0200

    add project files

---
+++ MouseCal/ui.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MouseCalibrator
+{
+    partial class MouseCal
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MouseCal));
+            this.mickeyInput = new System.Windows.Forms.NumericUpDown();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.checkBoxS = new System.Windows.Forms.CheckBox();
+            this.checkBoxC = new System.Windows.Forms.CheckBox();
+            this.checkBoxA = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mickeyInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mickeyInput
+            // 
+            this.mickeyInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.mickeyInput.BackColor = System.Drawing.SystemColors.Window;
+            this.mickeyInput.Location = new System.Drawing.Point(181, 12);
+            this.mickeyInput.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.mickeyInput.Name = "mickeyInput";
+            this.mickeyInput.Size = new System.Drawing.Size(58, 20);
+            this.mickeyInput.TabIndex = 0;
+            this.mickeyInput.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.mickeyInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SuppressEnter);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.resetBtn.Location = new System.Drawing.Point(181, 38);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(58, 30);
+            this.resetBtn.TabIndex = 2;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.ResetMickey);
+            // 
+            // checkBoxS
+            // 
+            this.checkBoxS.AutoSize = true;
+            this.checkBoxS.Checked = true;
+            this.checkBoxS.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxS.Location = new System.Drawing.Point(12, 12);
+            this.checkBoxS.Name = "checkBoxS";
+            this.checkBoxS.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkBoxS.Size = new System.Drawing.Size(47, 17);
+            this.checkBoxS.TabIndex = 3;
+            this.checkBoxS.Text = "Shift";
+            this.checkBoxS.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.checkBoxS.UseVisualStyleBackColor = true;
+            this.checkBoxS.CheckStateChanged += new System.EventHandler(this.CheckBoxUpdated);
+            // 
+            // checkBoxC
+            // 
+            this.checkBoxC.AutoSize = true;
+            this.checkBoxC.Location = new System.Drawing.Point(12, 35);
+            this.checkBoxC.Name = "checkBoxC";
+            this.checkBoxC.Size = new System.Drawing.Size(41, 17);
+            this.checkBoxC.TabIndex = 4;
+            this.checkBoxC.Text = "Ctrl";
+            this.checkBoxC.UseVisualStyleBackColor = true;
+            this.checkBoxC.CheckStateChanged += new System.EventHandler(this.CheckBoxUpdated);
+            // 
+            // checkBoxA
+            // 
+            this.checkBoxA.AutoSize = true;
+            this.checkBoxA.Location = new System.Drawing.Point(12, 58);
+            this.checkBoxA.Name = "checkBoxA";
+            this.checkBoxA.Size = new System.Drawing.Size(38, 17);
+            this.checkBoxA.TabIndex = 5;
+            this.checkBoxA.Text = "Alt";
+            this.checkBoxA.UseVisualStyleBackColor = true;
+            this.checkBoxA.CheckStateChanged += new System.EventHandler(this.CheckBoxUpdated);
+            // 
+            // MouseCal
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(251, 87);
+            this.Controls.Add(this.checkBoxA);
+            this.Controls.Add(this.checkBoxC);
+            this.Controls.Add(this.checkBoxS);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.mickeyInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MouseCal";
+            this.Text = "Mouse Calibrator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UI_Closing);
+            ((System.ComponentModel.ISupportInitialize)(this.mickeyInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown mickeyInput;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.CheckBox checkBoxS;
+        private System.Windows.Forms.CheckBox checkBoxC;
+        private System.Windows.Forms.CheckBox checkBoxA;
+    }
+}
---
+++ MouseCal/ui.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MouseCalibrator
+
+/*
+MouseCalibrator is a simple utility which can be used to fire off a set amount
+of mickeys.
+
+By finding a value that represents a 360 degree turn in first
+person games, this value can be used in other games to normalize the mouselook
+sensitivity independently of the operating system's adjusted speeds and
+acceleration.
+*/
+
+{
+    public partial class MouseCal : Form
+    /// <summary>
+    /// The Mousecal class contains the default values and handler functions
+    /// for the UI form.
+    /// </summary>
+    {
+
+        // Default value
+        private int mickeyValueDefault = 5611;
+
+        // Initial values
+        private int shiftState = 0;
+        private int ctrlState = 0;
+        private int altState = 0;
+
+        [DllImport("User32.dll", EntryPoint = "mouse_event", CallingConvention = CallingConvention.Winapi)]
+        internal static extern void Mouse_Event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        [DllImport("User32.dll", EntryPoint = "GetSystemMetrics", CallingConvention = CallingConvention.Winapi)]
+        internal static extern int InternalGetSystemMetrics(int value);
+
+        // https://msdn.microsoft.com/en-us/library/windows/desktop/ms646309(v=vs.85).aspx
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        // https://msdn.microsoft.com/en-us/library/windows/desktop/ms646327(v=vs.85).aspx
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4
+        }
+
+        // By assigning the arrow key enums to be mutually negative, the value
+        // can be used to multiply the input argument for Mouse_Event()
+        private const int leftArrow = -1;
+        private const int rightArrow = 1;
+
+        public MouseCal()
+        {
+            InitializeComponent();
+
+            mickeyInput.Value = mickeyValueDefault;
+
+            shiftState = SetModifierState(checkBoxS.Checked, KeyModifier.Shift);
+            ctrlState = SetModifierState(checkBoxC.Checked, KeyModifier.Control);
+            altState = SetModifierState(checkBoxA.Checked, KeyModifier.Alt);
+
+            UpdateModifierKeys(leftArrow, Keys.Left.GetHashCode());
+            UpdateModifierKeys(rightArrow, Keys.Right.GetHashCode());
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+            {
+                Mouse_Event(0x0001, m.WParam.ToInt32()*(int)mickeyInput.Value, 0, 0, 0);
+            }
+        }
+
+        private void UI_Closing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 0);
+            UnregisterHotKey(this.Handle, 1);
+        }
+
+        private void CheckBoxUpdated(object sender, EventArgs e)
+        {
+            CheckBox chk = (CheckBox)sender;
+            bool checkState = chk.Checked;
+
+            if (sender.Equals(checkBoxS))
+            {
+                this.shiftState = SetModifierState(checkState, KeyModifier.Shift);
+            }
+            if (sender.Equals(checkBoxC))
+            {
+                this.ctrlState = SetModifierState(checkState, KeyModifier.Control);
+            }
+            if (sender.Equals(checkBoxA))
+            {
+                this.altState = SetModifierState(checkState, KeyModifier.Alt);
+            }
+
+            UpdateModifierKeys(leftArrow, Keys.Left.GetHashCode());
+            UpdateModifierKeys(rightArrow, Keys.Right.GetHashCode());
+        }
+
+        private int SetModifierState(bool checkState, KeyModifier modifierKey)
+        {
+            if (checkState)
+            {
+                return (int)modifierKey;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        // ResetMickey resets the calibrator value
+        private void ResetMickey(object sender, EventArgs e)
+        {
+            mickeyInput.Value = mickeyValueDefault;
+        }
+
+        private void UpdateModifierKeys(int id, int keyHashCode)
+        {
+            RegisterHotKey(this.Handle, id, shiftState | this.ctrlState | this.altState, keyHashCode);
+        }
+
+        // Suppress windows sound notification on enter keypress
+        private void SuppressEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+    }
+}
